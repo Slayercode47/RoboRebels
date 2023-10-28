89eb4271d0d579dd68bd4ba963609358f60c7684
Slayercode47 <coolakashsingh4@gmail.com>
2023-10-28 23:22:28 +0530

    first commit

---
+++ Flower Plants/script.js
@@ -0,0 +1,62 @@
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function myFunction1(){
+
+    var y = document.getElementById("myNew");
+    if (y.style.display === "none") {
+        y.style.display = "block";
+    } else {
+        y.style.display = "none";
+    }
+}
+function myFunction2(){
+
+    var y = document.getElementById("mylight");
+    if (y.style.display === "none") {
+        y.style.display = "block";
+    } else {
+        y.style.display = "none";
+    }
+}
+function myFunction3(){
+
+    var y = document.getElementById("mysoil");
+    if (y.style.display === "none") {
+        y.style.display = "block";
+    } else {
+        y.style.display = "none";
+    }
+}
+function myFunction4(){
+
+    var y = document.getElementById("mywater");
+    if (y.style.display === "none") {
+        y.style.display = "block";
+    } else {
+        y.style.display = "none";
+    }
+}
+function myFunction5(){
+
+    var y = document.getElementById("myTH");
+    if (y.style.display === "none") {
+        y.style.display = "block";
+    } else {
+        y.style.display = "none";
+    }
+}
+function myFunction6(){
+
+    var y = document.getElementById("myfertilizer");
+    if (y.style.display === "none") {
+        y.style.display = "block";
+    } else {
+        y.style.display = "none";
+    }
+}
